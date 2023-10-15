9c2ccf4138d389ae9e4879d98dc30874b89a17a8
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-16 04:23:51 +0530

    Add project files.

---
+++ ViewStartInCoreMvc/Controllers/WorkerBookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewStartInCoreMvc.Controllers
+{
+    public class WorkerBookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
